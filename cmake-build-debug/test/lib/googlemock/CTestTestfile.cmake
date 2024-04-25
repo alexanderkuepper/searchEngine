83a3ab6b17a0da9fcf2e83d32b58ed5d9bfb4a6c
alex <kuepper.alexander@web.de>
2024-04-25 13:37:17 +0200

    delete file

--- cmake-build-debug/test/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alex/Desktop/SerachEngine with Tests/test/lib/googlemock
-# Build directory: C:/Users/alex/Desktop/SerachEngine with Tests/cmake-build-debug/test/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
